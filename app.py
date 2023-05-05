09adc8051a433a2b4a96922d44717157ecb40930
dingdingalicechang <128680974+dingdingalicechang@users.noreply.github.com>
2023-05-05 05:50:30 +0000

    05_05_afternoon

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template
+from flask import request
 
 app = Flask(__name__)
 @app.route("/")
@@ -8,9 +9,18 @@ def index():
         "key2":'''電金權值股中，台積電失守 500 元關卡，下跌 1%，聯發科逆勢上漲 1.3%，日月光也走強上漲 1%，不過航運股今日跌勢兇猛，長榮下挫 3.54%、萬海 4.27%、陽明跌 1.94%。''',
         "key3":'''高價股今日表現疲軟，祥碩下跌 3.9%、緯穎、世芯 - KY 下跌超過 2%，信驊、亞德客 - KY、創意下跌超過 1%，大立光、力旺、譜瑞 - KY 小漲作收，保瑞上漲 1% 以上。'''
     }
-        
+    
     return render_template("index.jinja.html",content_data=data)
 
-@app.route("/learning/")
-def learning():
-    return render_template("learning.jinja.html")
+
+@app.route("/features/")
+def features():
+    return render_template("features.jinja.html")
+
+@app.route("/priceing/")
+def priceing():
+    return render_template("priceing.jinja.html")
+
+@app.route("/about/")
+def about():
+    return render_template("about.jinja.html")
